617e03dede6041e386d9261f7a3bc0b851c3ac35
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2024-10-18 22:21:08 +0530

    meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import MetaPixel from "./_components/meta-pixel";
 
 const geistSans = localFont({
   src: "./fonts/AssistantVF.ttf",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <MetaPixel />
         {children}
       </body>
     </html>
